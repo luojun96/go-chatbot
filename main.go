380a941394482effb37bee53eb5e42a2d493ed3f
Jun Luo <luojun96@live.cn>
2023-06-18 17:18:22 +0800

    use openai go sdk to interact with chatgpt.

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"log"
 	"os"
 
@@ -18,30 +19,45 @@ var token Token
 // read token from config file `config.yaml`
 func readConfig() error {
 	log.Print("Reading config file...")
-	data, err := os.ReadFile("./config.yaml")
+	file, err := os.ReadFile("./config.yaml")
 	if err != nil {
 		log.Fatalf("failed reading data from file: %+v", err)
 		return err
 	}
 
-	if err = yaml.Unmarshal(data, &token); err != nil {
+	if err = yaml.Unmarshal(file, &token); err != nil {
 		log.Fatalf("failed unmarshaling data: %+v", err)
 		return err
 	}
 
 	log.Print("Reading config file success")
+	if token.Chatgpt3 == "" {
+		return errors.New("token is empty")
+	}
+
 	return nil
 }
 
 func interactGPT3(token string) {
 	client := openai.NewClient(token)
+	// resp, err := client.CreateCompletion(context.Background(),
+	// 	openai.CompletionRequest{
+	// 		Model:  openai.GPT3Davinci,
+	// 		Prompt: "This is a test",
+	// 	},
+	// )
+
+	// if err != nil {
+	// 	log.Fatalf("failed creating completion: %+v", err)
+	// }
+
 	resp, err := client.CreateChatCompletion(context.Background(),
 		openai.ChatCompletionRequest{
 			Model: openai.GPT3Dot5Turbo,
 			Messages: []openai.ChatCompletionMessage{
 				{
 					Role:    openai.ChatMessageRoleUser,
-					Content: "Hello, I'm a human",
+					Content: "Horses are my favorite",
 				},
 			},
 		},
@@ -50,7 +66,7 @@ func interactGPT3(token string) {
 		log.Fatalf("failed creating chat completion: %+v", err)
 	}
 
-	log.Printf("response: %+v", resp)
+	log.Printf("response: %+v", resp.Choices[0].Message.Content)
 }
 
 func main() {
